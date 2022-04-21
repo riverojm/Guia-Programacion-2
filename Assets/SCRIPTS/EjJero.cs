1cd01bba3d91b6b3e67a1cf5d2d21943d328eecc
juanma rivero <45921725+riverojm@users.noreply.github.com>
2022-04-21 15:37:54 -0300

    terminado

---
+++ Assets/SCRIPTS/EjJero.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjJero : MonoBehaviour
+{
+    public int numerogrande;
+    public int numerochico;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (numerogrande < numerochico || numerochico == numerogrande)
+        {
+            Debug.Log("Ingrese los numeros en el orden correcto, y que no sean iguales");
+        }
+        else
+        {
+            while (numerogrande != numerochico)
+            {
+                Debug.Log(""+numerogrande+"");
+                numerogrande--;
+            }
+            Debug.Log(""+numerogrande+"");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
